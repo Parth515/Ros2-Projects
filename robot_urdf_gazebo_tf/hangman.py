2f3c398873f008b6eee5c5cbd059f2c6dc7dc6e0
Parth515 <143266510+Parth515@users.noreply.github.com>
2024-04-14 17:27:34 +0200

    Add files via upload

---
+++ robot_urdf_gazebo_tf/hangman.py
@@ -0,0 +1,9 @@
+import random
+from collection import Counter
+
+someWords = '''apple banana mango strawberry orange grape pineapple apricot lemon coconut watermelon cherry papaya berry peach lychee muskmelon'''
+
+someWords = someWords.split(' ')
+word = random.choice(someWords)
+
+
